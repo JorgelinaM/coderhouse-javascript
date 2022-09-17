975e9d8852fe10cb7e53453500a91d06d861090c
Jorgelina Zarramera <jorgelinazarramera@hotmail.com>
2022-09-17 01:01:35 -0300

    JS-9 Incorporar eventos

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ user.age = prompt('Ingrese su edad');
 user.address = prompt('Ingrese su dirección');
 
 const h1 = document.getElementsByTagName('h1')[0];
-
 h1.innerText = h1.innerText + ' ' + user.name;
 
 const header = document.querySelector('header');
@@ -19,79 +18,25 @@ ul.append(ageLi);
 ul.append(addressLi);
 header.append(ul);
 
-const books = [];
+const bookInput = document.getElementById('book');
+const addBookButton = document.querySelector('.js-add-books');
+const booksList = document.querySelector('.js-books');
 
-function addBook() {
-    const book = prompt('Ingrese un libro');
-    books.push(book);
-}
-
-function removeLastBook() {
-    books.pop();
-}
-
-let shouldContinueBooks;
-while(shouldContinueBooks !== 'ESC') {
-    let response = prompt('¿Desea agregar un libro? Responda SI o NO');
-
-    while(response !== 'SI' && response !== 'NO') {
-        response = prompt('¿Desea agregar un libro? Responda solo SI o NO');
-    }
-    
-    if (response === 'SI') {
-        addBook()
-    }
-    
-    if (books.length > 0) { 
-        response = prompt('¿Desea remover el último libro? Responda SI o NO');
+addBookButton.addEventListener('click', function() {
+    if (bookInput.value !== '') {
+        const li = document.createElement('li');
+        li.innerText = bookInput.value;
         
-        while(response !== 'SI' && response !== 'NO') {
-            response = prompt('¿Desea remover el último libro? Responda solo SI o NO');
-        }
-
-        if (response === 'SI') {
-            removeLastBook()
-        }
+        const removeButton = document.createElement('button');
+        removeButton.innerText = 'Borrar';
+        
+        li.append(removeButton);
+        
+        booksList.append(li);
+        bookInput.value = '';
+        
+        removeButton.addEventListener('click', function() {
+            li.remove();
+        })
     }
-
-    if (books.length > 0) alert('Los libros ingresados son:' + books);
-
-    shouldContinueBooks = prompt('Presione ESC para salir o cualquier texto para continuar');
-}
-
-
-const searchBook = prompt('¿Desea buscar si existe el libro en el sistema? Escriba el nombre del libro a continuación')
-const doesBookExist = books.includes(searchBook);
-if (doesBookExist) alert('El libro ' + searchBook + ' se encuentra en el sistema');
-else alert('Lo sentimos. El libro ' + searchBook + ' no se encuentra en el sistema')
-
-function promptValues() {
-    let amount = parseFloat(prompt("Ingrese el monto a pagar de la subscripción"));
-    while(isNaN(amount)) {
-        amount = parseFloat(prompt("El monto debe ser un número. Ingrese nuevamente un valor."));
-    }
-
-    let quota = parseInt(prompt('¿Cuantas cuotas?'));
-    while(isNaN(quota)) {
-        quota = parseFloat(prompt("El monto debe ser un número. Ingrese nuevamente la cantidad de cuotas."));
-    }
-
-    return { amount, quota };
-}
-
-function paymentCalc() {    
-    const values = promptValues();
-    
-    const payment = values.amount / values.quota;
-    alert('El valor a pagar por mes es $' + payment);
-}
-
-paymentCalc();
-const continueMessage = 'Precione ESC para salir, de lo contrario el programa continuara';
-let shouldContinue = prompt(continueMessage);
-
-while (shouldContinue !== 'ESC') {
-    paymentCalc();
-
-    shouldContinue = prompt(continueMessage);
-}
+})
